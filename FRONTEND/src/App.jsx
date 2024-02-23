fbdc4ca9e973a4badf661328edd9e45e370ef251
Harsh Patel <harshpatel120605@gmail.com>
2024-02-23 15:43:29 +0530

    Add frontend files

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,16 @@
+import InputMenu from './inputMenu';
+import Login from './login';
+import SignUp from './signup';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            {/* <InputMenu/> */}
+            {/* <Login/> */}
+            <SignUp/>
+        </>
+    );
+}
+
+export default App;
