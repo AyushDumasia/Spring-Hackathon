04c73ad28d308bc657e64e07ac2130162e19315b
AYUSH DUMASIA <ayushdumasia00@gmail.com>
2024-03-01 22:25:09 +0530

    Customize Menu Page , add disable button

--- BACKEND/routes/home.js
+++ BACKEND/routes/home.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const isLoggedIn = require('../middlewares/isLoggedIn.js');
 const { getHome , postHome , subscription , menuPage , historyPage , addItem , addFeedback , getFeedback, getAttendance} = require('../controllers/homeController.js');
 const { adminValidation } = require('../middlewares/adminValidation.js');
+let Menu = require("../models/menuSchema.js")
 
 router.get("/", getHome);
 
@@ -22,4 +23,23 @@ router.post("/feedback" ,isLoggedIn, addFeedback)
 
 router.get("/attendance/:id",adminValidation, getAttendance);
 
+// router.get('/chef-history', async (req, res) => {
+//     try {
+//         let currentHour = new Date().getHours();
+//         console.log('Current Hour:', currentHour);
+//         let menu = await Menu.find({
+//             $expr: {
+//                 $eq: [
+//                     { $hour: '$Created_At' },
+//                     currentHour
+//                 ]
+//             }
+//         });
+//         console.log('Menu:', menu);
+//         res.status(200).json(menu);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
 module.exports = router;
