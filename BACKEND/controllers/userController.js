143e0a7796344607f60c66cb826dd6b9ac55a830
AYUSH DUMASIA <ayushdumasia00@gmail.com>
2024-03-06 21:51:37 +0530

    small changes

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -18,9 +18,9 @@ let signUp = (asyncHandler(async (req, res) => {
         let { username, email, phone, password } = req.body;
         let oldNumber = await User.findOne({phone});
         let oldEmail = await User.findOne({email});
-        if(password.lenght < 5){
+        if(password.length < 5){
             req.flash("failure" , "Password must be more than 5 characters long");
-            res.redirect("/sign-up");
+            return res.redirect("/sign-up");
         }
         if(oldNumber){
             req.flash("failure" , "Number already in use");
@@ -30,25 +30,25 @@ let signUp = (asyncHandler(async (req, res) => {
             req.flash("failure" , "Email already in use");
             return res.redirect("/sign-up");
         }
-        // let testAccount = await nodemailer.createTestAccount();
-        // const transporter = nodemailer.createTransport({
-        //     host: 'smtp.ethereal.email',
-        //     port: 587,
-        //     auth: {
-        //         user: 'dennis.bernier@ethereal.email',
-        //         pass: 'wRTtNZKGhH93Ky1zGz'
-        //     }
-        // });
+        let testAccount = await nodemailer.createTestAccount();
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'raleigh.hyatt@ethereal.email',
+                pass: 'qxte6Vsn7XJguT5f9h'
+            }
+        });
         
-        // const info = await transporter.sendMail({
-        //     from: '"Ayush Dumasia" <dennis.bernier@ethereal.email>', 
-        //     to: email,
-        //     subject: "Welcome..",
-        //     text: `You have successfully create your account in a Hostelly , Here is Your username : ${username} and Password : ${password}` ,
-        //     html: `<b>You have successfully create your account in a Hostelly , Here is Your username : ${username} and Password : ${password}</b>`,
-        // });
+        const info = await transporter.sendMail({
+            from: '"Ayush Dumasia" <raleigh.hyatt@ethereal.email>', 
+            to: email,
+            subject: "Welcome..",
+            text: `You have successfully create your account in a MessMeal , Here is Your username : ${username} and Password : ${password}` ,
+            html: `<b>You have successfully create your account in a MessMeal , Here is Your username : ${username} and Password : ${password}</b>`,
+        });
 
-        // console.log("Message sent: %s", info.messageId);
+        console.log("Message sent: %s", info.messageId);
         const newUser = new User({ username, email, phone });
         let registerUser = await User.register(newUser, password);
         req.login(registerUser, (err) => {
