c1fe85b6107ef3509237235caf4473f9aab74dfc
AYUSH DUMASIA <ayushdumasia00@gmail.com>
2024-03-14 15:29:05 +0530

    add files

--- BACKEND/controllers/inventory.js
+++ BACKEND/controllers/inventory.js
@@ -1,22 +1,22 @@
-const asyncHandler = require('express-async-handler');
-const Inventory = require('../models/inventorySchema.js')
-const { adminValidation } = require('../middlewares/adminValidation.js');
+const asyncHandler = require("express-async-handler");
+const Inventory = require("../models/inventorySchema.js");
+const { adminValidation } = require("../middlewares/adminValidation.js");
 
-let getInventory = asyncHandler(async(req ,res )=>{
-    res.render("./inventory/inventory.ejs" , {Inventory});
+let getInventory = asyncHandler(async (req, res) => {
+  res.render("./inventory/inventory.ejs", { Inventory });
 });
 
-let postInventory = asyncHandler(async (req ,res)=> {
-    let { product , quantity, perPrice } = req.body;
-    let newInventory = await Inventory.create({product , quantity , perPrice});
-    req.flash("success" , "Inventory added successfully")
-    res.redirect("/menu/inventory");
+let postInventory = asyncHandler(async (req, res) => {
+  let { product, quantity, perPrice } = req.body;
+  let newInventory = await Inventory.create({ product, quantity, perPrice });
+  req.flash("success", "Inventory added successfully");
+  res.redirect("/menu/inventory");
 });
 
-let inventoryHistory = asyncHandler(async(req , res) =>{
-    let inventory = await Inventory.find();
-    res.render("./inventory/inventoryHistory.ejs" , {inventory});
-    // res.send(inventory);
+let inventoryHistory = asyncHandler(async (req, res) => {
+  let inventory = await Inventory.find();
+  res.render("./inventory/inventoryHistory.ejs", { inventory });
+  // res.send(inventory);
 });
 
-module.exports = { getInventory , postInventory ,inventoryHistory }
+module.exports = { getInventory, postInventory, inventoryHistory };
--- BACKEND/controllers/pollController.js
+++ BACKEND/controllers/pollController.js
@@ -1,51 +1,52 @@
-const Poll = require('../models/pollSchema.js');
-const { adminValidation } = require('../middlewares/adminValidation.js');
-const asyncHandler = require('express-async-handler')
-const isLoggedIn = require('../middlewares/isLoggedIn.js');
-const PollResult = require('../models/pollResultSchema.js');
+const Poll = require("../models/pollSchema.js");
+const { adminValidation } = require("../middlewares/adminValidation.js");
+const asyncHandler = require("express-async-handler");
+const isLoggedIn = require("../middlewares/isLoggedIn.js");
+const PollResult = require("../models/pollResultSchema.js");
 
-let seePoll = (asyncHandler(async(req , res) =>{
-    let pollResults = await PollResult.find();
-    console.log(pollResults);
-}))
-
-
-
-let getPollMenu = (asyncHandler(async(req , res) => {
-    res.render("./poll/pollMenu.ejs");
-}));
-
-let postPollMenu = (asyncHandler(async(req , res ) => {
-    let { time , category_1 ,category_2 , category_3} = req.body;
-    let user = req.user;
-    let result = await Poll.create({time , category_1   , category_2 , category_3 , user });
-    // console.log(result);
-    req.flash("success" , "Poll created Successfully");
-    res.redirect("/menu/getPoll");
-}));
-
-
-let pollMenu = (asyncHandler(async(req , res)=>{
-    const latestPoll = await Poll.findOne().sort({ createdAt:-1 });
-    res.render("./poll/menuPoll.ejs" , {pollItem : latestPoll})
-}));
-
-
-const sendPoll = asyncHandler(async (req, res) => {
-    const latestPoll = await Poll.findOne().sort({ createdAt: -1 });
-    const user = req.user;
-    const { menu } = req.body;
-    if (!latestPoll) {
-        return res.status(404).json({ message: "No poll found" });
-    }
-    const pollResult = new PollResult({
-        Users: user._id,
-        items: menu,
-    });
-    await pollResult.save();
-    req.flash("success" , "Poll submitted successfully");
-    res.redirect("/menu/getPoll");
+let seePoll = asyncHandler(async (req, res) => {
+  let pollResults = await PollResult.find();
+  console.log(pollResults);
 });
 
+let getPollMenu = asyncHandler(async (req, res) => {
+  res.render("./poll/pollMenu.ejs");
+});
 
-module.exports = { seePoll ,getPollMenu , postPollMenu , pollMenu , sendPoll};
+let postPollMenu = asyncHandler(async (req, res) => {
+  let { time, category_1, category_2, category_3 } = req.body;
+  let user = req.user;
+  let result = await Poll.create({
+    time,
+    category_1,
+    category_2,
+    category_3,
+    user,
+  });
+  // console.log(result);
+  req.flash("success", "Poll created Successfully");
+  res.redirect("/menu/getPoll");
+});
+
+let pollMenu = asyncHandler(async (req, res) => {
+  const latestPoll = await Poll.findOne().sort({ createdAt: -1 });
+  res.render("./poll/menuPoll.ejs", { pollItem: latestPoll });
+});
+
+const sendPoll = asyncHandler(async (req, res) => {
+  const latestPoll = await Poll.findOne().sort({ createdAt: -1 });
+  const user = req.user;
+  const { menu } = req.body;
+  if (!latestPoll) {
+    return res.status(404).json({ message: "No poll found" });
+  }
+  const pollResult = new PollResult({
+    Users: user._id,
+    items: menu,
+  });
+  await pollResult.save();
+  req.flash("success", "Poll submitted successfully");
+  res.redirect("/menu/getPoll");
+});
+
+module.exports = { seePoll, getPollMenu, postPollMenu, pollMenu, sendPoll };
